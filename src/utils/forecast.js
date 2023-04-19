dddd170665fd9f9bde279808dc6ddeea37fd102d
plana <emirplanic1@gmail.com>
2023-04-19 08:58:03 +0200

    add humidity and about page description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude,longitude,callback) =>{
-    const url = 'http://api.weatherstack.com/current?access_key=9f341793eda2f0b55de27c61814f768c&query='+ latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=9f341793eda2f0b55de27c61814f768c&query='+ latitude + ',' + longitude + '&units'
 
     request({url,json:true},(error,{body})=>{
                 if(error){
@@ -9,7 +9,8 @@ const forecast = (latitude,longitude,callback) =>{
                 }else if (body.error){
                 callback('Unable to find location',undefined)
                 }else {
-                    callback(undefined,body.current.weather_descriptions[0] + '. Its currently '  + body.current.temperature + ' is current temperature but it feels like ' + body.current.feelslike)
+                    
+                    callback(undefined,body.current.weather_descriptions[0] + '. Its currently '  + body.current.temperature + ' °C ,and it feels like ' + body.current.feelslike + ' °C. The humidity is ' + body.current.humidity + '%')
                 }
             })
 }
